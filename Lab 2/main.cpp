02a462a0bc7ea249009b856518f4b53164499ccc
Zaina Qasim <48165574+qasimza@users.noreply.github.com>
2019-06-01 17:22:48 -0400

    Add files via upload

---
+++ Lab 2/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double hours, minutes, time, ft;
+	cout << "Enter Hours: ";
+	cin >> hours;
+	cout << endl << "Enter Minutes: ";
+	cin >> minutes;
+	time = hours + minutes / 60;
+	ft = 4 * time*time / (time + 2) - 20;
+	cout << endl << "Approximate Temperature: " << ft << endl;
+	return 0;
+}
