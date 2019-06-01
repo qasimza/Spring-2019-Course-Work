12add82b13f7b3aa4f4b93516b7ed04de6f424a4
Zaina Qasim <48165574+qasimza@users.noreply.github.com>
2019-06-01 17:26:03 -0400

    Add files via upload

---
+++ Lab 2/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x, y;
+	cout << "Enter XY Pair: ";
+	cin >> x;
+	cin >> y;
+	cout << endl;
+	if (x > 0 && y > 0)
+		cout << "Pair is Q1" << endl;
+	else if (x < 0 && y > 0)
+		cout << "Pair is Q2" << endl;
+	else if (x < 0 && y < 0)
+		cout << "Pair is Q3" << endl;
+	else if (x > 0 && y < 0)
+		cout << "Pair is Q4" << endl;
+	else if (x == 0 && y == 0)
+		cout << "Center Point" << endl;
+	else if (x == 0)
+		cout << "On Y-Axis" << endl;
+	else 
+		cout << "On X-Axis" << endl;
+	return 0;
+}
